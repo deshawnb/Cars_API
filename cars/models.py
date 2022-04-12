ea6337ae046eca580a1fcdd1a09ce99c265646b3
Deshawn Brown <brownd726@gmail.com>
2022-04-12 15:57:29 -0500

    created car serializer and file

--- cars/models.py
+++ cars/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Cars(models.Model):
+class Car(models.Model):
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     year = models.IntegerField()
